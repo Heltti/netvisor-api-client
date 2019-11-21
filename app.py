26a9445ee26a9b34c3431647688bb9e4e9e73dff
Tristen Tooming <tristen.tooming@gmail.com>
2019-11-21 15:00:28 +0200

    return dict for cases sales: count overdue, overdue sum, count open , count rejected. For purchase: count open

--- app.py
+++ app.py
@@ -13,11 +13,28 @@ client = Netvisor(
 '''
 Api ei handlaa nyt substatusta osto- ja myyntilaskuissa
 '''
-# Erääntyneet
-overdue_list = client.sales_invoices.list(status='overdue')
-print(len(overdue_list))
-sum = 0
-for invoice in overdue_list:
-    sum = sum + invoice['sum']
+data = {'Myyntilaskut': {},
+        'Ostolaskut': {}}
+### Myyntlaskut
+# Erääntyneet, hakee seuraavat substatusalaiset OVERDUE, REMINDED, REQUESTED, COLLECTED
+overdue_sales_list = client.sales_invoices.list(status='overdue')
+data['Myyntilaskut']['Erääntyneiden myyntilaskujen määrä'] = len(overdue_sales_list)
 
-print(sum)
+sum = 0
+for invoice in overdue_sales_list:
+    sum = sum + invoice['sum']
+data['Myyntilaskut']['Erääntyneiden summa'] = sum
+
+# Avoimet, OPEN, OVERDUE, REMINDED, REQUESTED, COLLECTED
+open_sales_list = client.sales_invoices.list(status='open')
+data['Myyntilaskut']['Avoimien määrä'] = len(open_sales_list)
+
+# Hylätyt
+rejected_sales_list = client.sales_invoices.list(status='rejected')
+data['Myyntilaskut']['Hylättyjen määrä'] = len(rejected_sales_list)
+
+### Ostolaskut
+open_purchase_list = client.purchase_invoices.list(status='Open')
+data['Ostolaskut']['Avointen määrä'] = len(open_purchase_list)
+
+print(data)
