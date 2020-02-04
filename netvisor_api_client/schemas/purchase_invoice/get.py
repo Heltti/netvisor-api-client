2ebdaa59c345cb7da5033e448b6a0b3bc596606d
tristen-tooming <tristen.tooming@mankies.fi>
2020-02-04 17:02:55 +0200

    Dimensions and vat_percatage needs editing

--- netvisor_api_client/schemas/purchase_invoice/get.py
+++ netvisor_api_client/schemas/purchase_invoice/get.py
@@ -12,17 +12,20 @@ class VatPercentageSchema(Schema):
 class PurchaseInvoiceLineSchema(Schema):
     netvisor_key = fields.Integer(load_from="netvisorkey")
     line_sum = Decimal()
+    line_net_sum = Decimal()
     unit_price = Decimal()
-    vat_percentage = fields.Nested(VatPercentageSchema, load_from='vat_percent')
+    vat_percentage = fields.Nested(VatPercentageSchema)
     vat_code = fields.String()
     description = fields.String()
     unit = fields.String()
     order_amount = fields.Integer()
-    purchase_price = Decimal()
-    delivered_amount = fields.Integer()
+    purchase_price = Decimal(allow_none=True)
+    delivered_amount = Decimal()
     product_code = fields.String()
     discount_percentage = Decimal()
     product_name = fields.String()
+    bookkeeping_account_netvisor_key = fields.String(load_from='accounting_suggestion_bookkeeping_account_netvisor_key')
+    bookkeeping_account = fields.String(load_from='accounting_suggestion_bookkeeping_account')
 
 
 class InvoiceLineSchema(Schema):
@@ -37,22 +40,13 @@ class InvoiceLineSchema(Schema):
             return input_data['invoice_lines']
 
 
-class InvoiceLinesSchema(Schema):
-    invoice_line = fields.Nested(InvoiceLineSchema)
-
-    @post_load
-    def preprocess_invoice_lines(self, input_data):
-        if input_data:
-            return input_data['invoice_line']
-
-
 class PurchaseInvoiceDimensionSchema(Schema):
     """
     Levels 4 -5 -6 when version parameter is2
     In other case levels 3-4-5
     """
-    dimension_name = fields.String()
-    dimension_detail_name = fields.String()
+    name = fields.String(load_from='dimension_name')
+    detail_name = fields.String(load_from='dimension_detail_name')
 
 
 class InvoiceDimensionSchema(Schema):
@@ -67,44 +61,34 @@ class InvoiceDimensionSchema(Schema):
             return input_data['dimensions']
 
 
-class InvoiceDimensionsSchema(Schema):
-    invoice_dimension = List(fields.Nested(InvoiceDimensionSchema))
-
-    @post_load
-    def preprocess_invoice_dimension(self, input_data):
-        if input_data:
-            return input_data['invoice_dimension']
-
-
 class GetPurchaseInvoiceSchema(Schema):
     netvisor_key = fields.Integer(load_from='purchase_invoice_netvisor_key')
-    number = fields.String(load_from='purchase_invoice_number')
+    number = fields.Integer(load_from='purchase_invoice_number')
     date = fields.Nested(DateSchema, load_from='purchase_invoice_date')
     delivery_date = fields.Nested(DateSchema, load_from='purchase_invoice_delivery_date')
     due_date = fields.Nested(DateSchema, load_from='purchase_invoice_due_date')
     value_date = fields.Nested(DateSchema, load_from='purchase_invoice_value_date')
-    reference_number = fields.String(load_from='purchase_invoice_reference_number')
+    reference_number = fields.String(load_from='purchase_invoice_referencenumber')
     amount = fields.Nested(DecimalSchema(), load_from='purchase_invoice_amount')
     paid_amount = fields.Nested(DecimalSchema(), load_from='purchase_invoice_paid_amount')
     foreign_currency_amount = fields.Nested(DecimalSchema())
     foreign_currency_name_id = fields.String()
     status = fields.String(load_from='invoice_status')
+    approval_status = fields.String()
     our_reference = fields.String(allow_none=True, load_from='purchase_invoice_our_reference')
     your_reference = fields.String(allow_none=True, load_from='purchase_invoice_your_reference')
     description = fields.String(allow_none=True, load_from='purchase_invoice_description')
     vendor_name = fields.String()
-    vendor_addresline = fields.String(allow_none=True)
+    vendor_addressline = fields.String(allow_none=True)
     vendor_postnumber = fields.String(allow_none=True)
     vendor_town = fields.String(allow_none=True)
     vendor_country = fields.String(allow_none=True)
+    vendor_bank_account_number = fields.String(allow_none=True)
     voucher_id = fields.Integer(allow_none=True)
     accounted = fields.Boolean(load_from='is_accounted')
     comment = fields.String()
-    try:
-        invoice_lines = fields.Nested(InvoiceLinesSchema)
-        dimensions = fields.Nested(InvoiceDimensionsSchema)
-    except:
-        print("paskaa")
+    invoice_lines = fields.Nested(InvoiceLineSchema)
+    dimensions = fields.Nested(InvoiceDimensionSchema)
 
 
     class Meta:
