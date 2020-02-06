ae6d8002c24b3d222e51097e62ab1d30dc70a7a6
tristen-tooming <tristen.tooming@mankies.fi>
2020-02-06 14:13:12 +0200

    Reformatted file

--- netvisor_api_client/schemas/purchase_invoice/get.py
+++ netvisor_api_client/schemas/purchase_invoice/get.py
@@ -5,22 +5,24 @@ from ..fields import Boolean, Decimal, List
 
 
 class PurchaseInvoiceLineSchema(Schema):
-    netvisor_key = fields.Integer(load_from="netvisorkey")
-    line_sum = Decimal()
-    line_net_sum = Decimal()
-    unit_price = Decimal()
-    vat_percent = Decimal()
-    vat_code = fields.String()
-    description = fields.String()
-    unit = fields.String()
-    ordered_amount = Decimal()
+    netvisor_key = fields.Integer(required=True, load_from="netvisorkey")
+    line_sum = Decimal(required=True)
+    line_net_sum = Decimal(required=True)
+    unit_price = Decimal(required=True)
+    vat_percent = Decimal(required=True)
+    vat_code = fields.String(required=True)
+    description = fields.String(required=True)
+    unit = fields.String(required=True)
+    ordered_amount = Decimal(required=True)
     purchase_price = Decimal(allow_none=True)
-    delivered_amount = Decimal()
-    product_code = fields.String()
-    discount_percentage = Decimal()
-    product_name = fields.String()
-    bookkeeping_account_netvisor_key = fields.String(load_from='accounting_suggestion_bookkeeping_account_netvisor_key')
-    bookkeeping_account = fields.String(load_from='accounting_suggestion_bookkeeping_account')
+    delivered_amount = Decimal(required=True)
+    product_code = fields.String(required=True)
+    discount_percentage = Decimal(required=True)
+    product_name = fields.String(required=True)
+    bookkeeping_account_netvisor_key = fields.String(required=True,
+                                                     load_from='accounting_suggestion_bookkeeping_account_netvisor_key')
+    bookkeeping_account = fields.String(required=True,
+                                        load_from='accounting_suggestion_bookkeeping_account')
 
 
 class PurchaseInvoiceDimensionSchema(Schema):
@@ -28,10 +30,11 @@ class PurchaseInvoiceDimensionSchema(Schema):
     Levels 4 -5 -6 when version parameter is2
     In other case levels 3-4-5
     """
-    name = fields.String(load_from='dimension_name')
-    netvisor_key = fields.Integer(load_from='dimension_name_netvisor_key')  # Not in documentation
-    detail_name = fields.String(load_from='dimension_detail_name')
-    detail_netvisor_key = fields.Integer(load_from='dimension_detail_netvisor_key') # Not in documentation
+    name = fields.String(required=True, load_from='dimension_name')
+    netvisor_key = fields.Integer(required=True, load_from='dimension_name_netvisor_key')  # Not in documentation
+    detail_name = fields.String(required=True, load_from='dimension_detail_name')
+    detail_netvisor_key = fields.Integer(required=True,
+                                         load_from='dimension_detail_netvisor_key')  # Not in documentation
 
 
 class InvoiceDimensionLineSchema(Schema):
@@ -58,8 +61,8 @@ class InvoiceLineSchema(Schema):
 
 
 class PurchaseOrderSchema(Schema):
-    number = fields.Integer(load_from='order_number')
-    netvisor_key = fields.Integer()
+    number = fields.Integer(required=True, load_from='order_number')
+    netvisor_key = fields.Integer(required=True)
 
 
 class LinkedPurchaseOrdersSchema(Schema):
@@ -75,35 +78,110 @@ class LinkedPurchaseOrdersSchema(Schema):
 
 
 class GetPurchaseInvoiceSchema(Schema):
-    netvisor_key = fields.Integer(load_from='purchase_invoice_netvisor_key')
-    number = fields.Integer(load_from='purchase_invoice_number')
-    date = fields.Nested(DateSchema, load_from='purchase_invoice_date')
-    delivery_date = fields.Nested(DateSchema, load_from='purchase_invoice_delivery_date')
-    due_date = fields.Nested(DateSchema, load_from='purchase_invoice_due_date')
-    value_date = fields.Nested(DateSchema, load_from='purchase_invoice_value_date')
-    reference_number = fields.String(load_from='purchase_invoice_referencenumber')
-    amount = fields.Nested(DecimalSchema(), load_from='purchase_invoice_amount')
-    paid_amount = fields.Nested(DecimalSchema(), load_from='purchase_invoice_paid_amount')
-    foreign_currency_amount = fields.Nested(DecimalSchema())
-    foreign_currency_name_id = fields.String()
-    status = fields.String(load_from='invoice_status')
-    approval_status = fields.String()
-    our_reference = fields.String(allow_none=True, load_from='purchase_invoice_our_reference')
-    your_reference = fields.String(allow_none=True, load_from='purchase_invoice_your_reference')
-    description = fields.String(allow_none=True, load_from='purchase_invoice_description')
-    vendor_name = fields.String()
-    vendor_addressline = fields.String(allow_none=True)
-    vendor_postnumber = fields.String(allow_none=True)
-    vendor_town = fields.String(allow_none=True)
-    vendor_country = fields.String(allow_none=True)
-    vendor_bank_account_number = fields.String(allow_none=True)
+    netvisor_key = fields.Integer(
+        required=True,
+        load_from='purchase_invoice_netvisor_key'
+    )
+    number = fields.Integer(
+        required=True,
+        allow_none=True,
+        load_from='purchase_invoice_number'
+    )
+    date = fields.Nested(
+        DateSchema,
+        required=True,
+        load_from='purchase_invoice_date'
+    )
+    delivery_date = fields.Nested(
+        DateSchema,
+        required=True,
+        load_from='purchase_invoice_delivery_date'
+    )
+    due_date = fields.Nested(
+        DateSchema,
+        required=True,
+        load_from='purchase_invoice_due_date'
+    )
+    value_date = fields.Nested(
+        DateSchema,
+        required=True,
+        load_from='purchase_invoice_value_date'
+    )
+    reference_number = fields.String(
+        required=True,
+        load_from='purchase_invoice_referencenumber'
+    )
+    amount = fields.Nested(
+        DecimalSchema(),
+        required=True,
+        load_from='purchase_invoice_amount'
+    )
+    paid_amount = fields.Nested(
+        DecimalSchema(),
+        required=True,
+        allow_none=True,
+        load_from='purchase_invoice_paid_amount'
+    )
+    foreign_currency_amount = fields.Nested(
+        DecimalSchema(),
+        required=True
+    )
+    foreign_currency_name_id = fields.String(required=True)
+    status = fields.String(
+        required=True,
+        load_from='invoice_status')
+    approval_status = fields.String(required=True)
+    our_reference = fields.String(
+        required=True,
+        allow_none=True,
+        load_from='purchase_invoice_our_reference'
+    )
+    your_reference = fields.String(
+        required=True,
+        allow_none=True,
+        load_from='purchase_invoice_your_reference'
+    )
+    description = fields.String(
+        required=True,
+        allow_none=True,
+        load_from='purchase_invoice_description'
+    )
+    vendor_name = fields.String(required=True)
+    vendor_addressline = fields.String(
+        required=True,
+        allow_none=True
+    )
+    vendor_postnumber = fields.String(
+        required=True,
+        allow_none=True
+    )
+    vendor_town = fields.String(
+        required=True,
+        allow_none=True
+    )
+    vendor_country = fields.String(
+        required=True,
+        allow_none=True
+    )
+    vendor_bank_account_number = fields.String(
+        required=True,
+        allow_none=True
+    )
     voucher_id = fields.Integer(allow_none=True)
-    accounted = fields.Boolean(load_from='is_accounted')
-    preview_image = fields.String(allow_none=True)
-    attachments = fields.Nested()
-    comment = fields.String()
-    lines = fields.Nested(InvoiceLineSchema, load_from='invoice_lines')
-    orders = fields.Nested(LinkedPurchaseOrdersSchema, load_from='linked_purchase_orders')
+    accounted = fields.Boolean(
+        required=True,
+        load_from='is_accounted'
+    )
+    preview_image = fields.String(
+        allow_none=True)
+    lines = fields.Nested(
+        InvoiceLineSchema,
+        load_from='invoice_lines'
+    )
+    orders = fields.Nested(
+        LinkedPurchaseOrdersSchema,
+        load_from='linked_purchase_orders'
+    )
 
     @post_load
     def preprocess_lines(self, data):
