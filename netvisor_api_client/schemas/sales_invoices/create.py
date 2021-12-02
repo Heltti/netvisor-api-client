25ba3ea2fa0079b264ffa7015a7e624f376d79dd
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2021-12-02 21:03:16 +0200

    [IMP] Add invoice event date

--- netvisor_api_client/schemas/sales_invoices/create.py
+++ netvisor_api_client/schemas/sales_invoices/create.py
@@ -103,6 +103,7 @@ class CreateSalesInvoiceSchema(RejectUnknownFieldsSchema):
     sales_invoice_number = fields.Integer(attribute='number')
     sales_invoice_date = fields.Date(attribute='date')
     sales_invoice_value_date = fields.Date(attribute='value_date')
+    sales_invoice_event_date = fields.Date(attribute='event_date')
     sales_invoice_delivery_date = fields.Date(attribute='delivery_date')
     sales_invoice_reference_number = fields.String(attribute='reference_number')
     sales_invoice_amount = Decimal(attribute='amount')
